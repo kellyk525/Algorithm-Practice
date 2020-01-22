e5165077483ad4ff8495e4dfedc4bce9b9373e36
Kelly <kjk224@cornell.edu>
2020-01-21 21:39:10 -0800

    practice

--- allproblems.js
+++ allproblems.js
@@ -180,7 +180,299 @@ var moveZeroes = function(n) {
     }
 }
 
+// subsets
+// permutations
+// palindrome
+// binary search
+// merge sort
+// quick sort
+
+
 //  Node / Linked List problems ---------------
 
-
 //  Deleting Node
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    addToTail(value) {
+        let node = this;
+        
+        while (node.next) {
+            node = node.next;
+        }
+
+        node.next = Node(value);
+    }
+
+    getValue(index) {
+        let node = this;
+
+        while(node && index) {
+            node = node.next;
+            index = index - 1;
+        }
+
+        if (node) {
+            return node.value;
+        } else {
+            return false;
+        }
+
+    }
+
+    deleteByValue(value) {
+        let node = this;
+
+        while (node && node.next) {
+
+            if (node.next === value) {
+                node.next = node.next.next;
+            }
+
+            node = node.next; 
+            // goes to the new value that was set on node.next
+        }
+    }
+
+
+    reverseLinkedList(root) {
+
+        if (!root) return false;
+
+        let reversed = [];
+        let node = root;
+
+        while (node) {
+            reversed.unshift(node.val);
+            node = node.next;
+        }
+
+        for (let i = 0; i < reversed.length; i ++) {
+            let from = result[i];
+            let to = result[i+1];
+
+            if (to === undefined) to = null;
+            from.next = to;
+        }
+
+        return reversed[0];
+    }
+
+    //  Remove dups from a sorted list
+
+    deleteDups(head) {
+        let node = head;
+
+        while (node) {
+            if (node.next && node.next.val === node.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return node;
+    }
+
+    // Detecting Cycle
+
+    hasCycle(head) {
+        let slow = head;
+        let fast = head;
+
+        while (slow && fast) {
+            slow = slow.next;
+            fast = fast.next ? fast.next.next : undefined;
+            if (slow === fast) return true;
+        }
+        return false;
+    }
+
+    //  Remove Nth Node from End of List
+
+    //  Reorder List
+
+}
+
+// Tree Problems
+
+class Tree {
+    constructor (data) {
+        this.data = data;
+        this.children = [];
+    }
+
+    addNode(data) {
+        const node = new Tree(data);
+        this.children.push(node);
+    }
+
+    remove(data) {
+        this.children = this.children.filter(node => {
+            return node.data !== data;
+        })
+    }
+
+    traverseBF(fn) {
+        let final = [this.data];
+
+        while (final.length) {
+            let node = final.shift();
+            final.push(...node.children);
+            fn(node);
+        }
+    }
+
+    traverseDF(fn) {
+        let final = [this.data];
+
+        while (final.length) {
+            let node = final.shift();
+            final.unshift(...node.children);
+            fn(node);
+        }
+
+    }
+
+    preOrderTraversal() {
+
+    }
+
+    postOrderTraversal() {
+
+
+    }
+
+    minimumDepth() {
+
+    }
+    // N-ary tree
+
+}
+
+
+// Binary Tree Problems
+
+class BinaryTree {
+
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    // Insert data
+
+    // Contains data
+
+    // Level Height
+
+    // Inorder 
+
+    inOrderTraversal(root) {
+        let final = [];
+        dfs(root);
+
+        function dfs(node) {
+
+            if (node) {
+                dfs(node.left);
+                final.push(node.val);
+                dfs(node.right);
+            }
+        }
+
+        return final;
+    }
+
+    // Preorder
+
+    preOrderTraversal(root) {
+
+        let final = [];
+        helper(root);
+
+        function helper(node) {
+            if (node) {
+                final.push(node.val);
+                helper(node.left);
+                helper(node.right);
+            }
+        }
+
+        return final;
+    }
+
+    // Postorder
+
+    postOrderTraversal(root) {
+
+        let final = [];
+        helper(root);
+
+        function helper(node) {
+            if (node) {
+                helper(node.left);
+                helper(node.right);
+                final.push(node.val);
+            }
+        }
+
+        return final;
+    }
+
+    // MaxDepth
+
+    maxDepth(root) {
+        let depth = 0;
+        helper(root, 1);
+        return depth;
+
+        function helper(node, height) {
+            if (node) {
+                depth = Math.max(depth, height);
+                helper(node.left, height +1);
+                helper(node.right, height +1);
+
+            }
+        }
+    }
+
+    maxDepth(root) {
+        helper(root, 0);
+
+        function helper(node, height) {
+
+            if (!node) return height;
+            return Math.max(helper(node.left, height + 1), helper(node.right, height + 1))
+        }
+    }
+
+    // MinDepth
+
+    minDepth(root) {
+
+        function helper(root, height) {
+            if (node) {
+                helper(node.left, )
+            }
+        }
+
+    }
+
+    // Largest Number
+
+
+
+    // isBalanced
+
+    // Invert
+
+    // Swap
+
+    // Binary Tree Right Side View
+}
